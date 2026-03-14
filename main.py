09c0748fefb3b6afd0c675da6a24c00b1815e3e6
Ahmad <mshakeel44514451@gmail.com>
2026-03-14 14:35:00 +0500

    main function is made and search query added and formatted

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from playwright.sync_api import sync_playwright
 import pandas as pd
 import time
+
+def scrape_google_maps(search_query):
+  leads=[]
+
+  formatted_query=search_query.replace(" ","+")
+  target_url=f"https://www.google.com/maps/search/{formatted_query}"
+
+  
