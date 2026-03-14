93262c61ec77faad458bbf3a78f3399781016ba2
Ahmad <mshakeel44514451@gmail.com>
2026-03-14 16:01:29 +0500

    Browser is launched, new profie with customized viewport is created and new page is made

--- main.py
+++ main.py
@@ -8,4 +8,11 @@ def scrape_google_maps(search_query):
   formatted_query=search_query.replace(" ","+")
   target_url=f"https://www.google.com/maps/search/{formatted_query}"
 
-  
+  with sync_playwright() as p:
+    print(f"🗺️ Launching Maps Harvester for: '{search_query}'...")
+    browser=p.chromium.launch(headless=False)
+    context=browser.new_context(viewport={"width":1920,"height":1080},locale="en-US")
+    page=context.new_page()
+
+    print(f"🌍 Navigating to Google Maps...")
+    page.goto(target_url,timeout=60000)
